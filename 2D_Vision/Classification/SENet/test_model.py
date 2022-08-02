78f256025e751e84fd7fd255fb6e2240e87e4a05
john <tmvkrorl2@gmail.com>
2022-08-02 12:15:16 +0900

    mobileNet, resnet add

--- 2D_Vision/Classification/SENet/test_model.py
+++ 2D_Vision/Classification/SENet/test_model.py
@@ -31,19 +31,5 @@ def se_resnetX101():
 
     return summary(model, input_size=(3, 224, 224))
 
-#
-# def resnet152():
-#     model = ResNet50(Res_bottleneck, [3, 8, 36, 3], n_classes=10)
-#     print(model)
-#
-#     # 모델을 CUDA로 전달합니다.
-#     model.to("cuda")
-#     print(next(model.parameters()).device)
-#
-#     # 모델 summary를 확인합니다.
-#
-#     return summary(model, input_size=(3, 224, 224))
-
 
 se_resnetX50()
-# resnetX101()
